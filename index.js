5c197343c9711c3a960661c39d8696239d98b413
Melissa <melissa_hartung@teilnehmer.cdemy.de>
2025-07-28 14:14:17 +0200

    structure

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-function add (num1, num2, num3 = 0) {
-    const ergebnis = num1 + num2 + num3;
-    alert('das ergebnis ist ' + ergebnis)
-}
+let heute= new Date();
+let month= heute.getMonth(); // Monate sind 0-basiert
 
-// add(1, 1, 1);
-// add(1, 2, 3);
+
+
+// function add (num1, num2, num3 = 0) {
+//     const ergebnis = num1 + num2 + num3;
+//     alert('das ergebnis ist ' + ergebnis)
+// }
+
+// // add(1, 1, 1);
+// // add(1, 2, 3);
 
  
 
-const listOfTDElements = document.getElementsByTagName("td")
-const background = document.getElementsByClassName("notesBackground")[0];
+// const listOfTDElements = document.getElementsByTagName("td")
+// const background = document.getElementsByClassName("notesBackground")[0];
 
-background.addEventListener("click", function() {
-    background.style.display = "none";
-});
-
-
-Array.from(listOfTDElements).forEach(element => {
-    element.addEventListener("click", function() {
-        background.style.display = "block";XMLDocument
-    });
-});
+// background.addEventListener("click", function() {
+//     background.style.display = "none";
+// });
 
 
-document.createElement("textarea");
+// Array.from(listOfTDElements).forEach(element => {
+//     element.addEventListener("click", function() {
+//         background.style.display = "block";XMLDocument
+//     });
+// });
+
+
+// document.createElement("textarea");
