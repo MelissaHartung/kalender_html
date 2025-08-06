8970b003967887b86168faf3e75929dd3714b219
Melissa <melissa_hartung@teilnehmer.cdemy.de>
2025-08-06 14:58:32 +0200

    put changemonthbutton

--- Kalender.js
+++ Kalender.js
@@ -1,27 +1,3 @@
-function getCustomDayIndex(date) {
-  const day = date.getDay(); // JS: So=0, Mo=1, Di=2, ...
-  return (day + 6) % 7; // verschiebt so, dass Mo=0, Di=1, ..., So=6
-}
-// Wochentage
-let shiftedWeekday = getCustomDayIndex(today);
-let daysFromMonday = "";
-if (shiftedWeekday === 0) {
-  daysFromMonday = "Montag";
-} else if (shiftedWeekday === 1) {
-  daysFromMonday = "Dienstag";
-} else if (shiftedWeekday === 2) {
-  daysFromMonday = "Mittwoch";
-} else if (shiftedWeekday === 3) {
-  daysFromMonday = "Donnerstag";
-} else if (shiftedWeekday === 4) {
-  daysFromMonday = "Freitag";
-} else if (shiftedWeekday === 5) {
-  daysFromMonday = "Samstag";
-} else if (shiftedWeekday === 6) {
-  daysFromMonday = "Sonntag";
-}
-
-// function generateCalendar(year, month) {
 //   const weekdayOfFirstOfMonth = new Date(year, month, 1).getDay();
 //   const lastDayOfMonth = new Date(year, month + 1, 1 - 1).getDate();
 //   const offset = weekdayOfFirstOfMonth - 1;
@@ -44,7 +20,7 @@ if (shiftedWeekday === 0) {
 //     tablebody.appendChild(tablerow);
 //   }
 // }
-// generateCalendar(Year, Month);
+generateCalendar(Year, Month);
 
 let Jahr = new Date().getFullYear();
 let Monat = new Date().getMonth();
@@ -72,6 +48,7 @@ function generateCalendar(year, month) {
       } else {
         tablecell.innerText = currentDate;
       }
+
       tablerow.appendChild(tablecell);
       currentDate++;
     }
@@ -91,5 +68,29 @@ function changeMonth(delta) {
   generateCalendar(Jahr, Monat);
 }
 
-// Starte mit dem aktuellen Monat
-generateCalendar(Jahr, Monat);
+// let heute = generateCalendar(Jahr, Monat);
+
+// function generateCalender(year, month) {
+//   const firstday = new Date(year, month, 1);
+//   let weekdayofFirstOfMonth = firstday.getDay();
+//   weekdayOfFirstOfMonth = (weekdayOfFisrtOfMonth + 6) % 7;
+//   const lastDayOfMonth = new date(year, month + 1, 0).getDate();
+//   const tablebody = document.getElementById("clickelement");
+
+//   tablebody.innerHTML = "";
+//   let currentDate = 1 - weekdayOfFirstOfMonth; // 1- der wochentag des ersten, damit dieser dann <1 nicht angezeigt wird
+
+//   for (let week = 0; week < 6; week++) {
+//     const tablerow = document.createElement("tr");
+//     for (let day = 0; day < 7; day++) {
+//       const tablecell = document.createElemnt("td");
+
+//       if (currentDate < 1 || currentDate > lastDayOfMonth) {
+//         //ist das currentDate (= Ergebnis von 1- weekdayOfFirstOfMonth)kleiner als 1 und ist CurrentDate größer als der 30/31, dann lass die zeile leer
+//         tablecell.innerHTML = "";
+//       } else {
+//         tablecell.innerText = currentDate;
+//       }
+//     }
+//   }
+// }
